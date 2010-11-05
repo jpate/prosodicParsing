1806e00349239e9bd434bfecc6e72c2762fed045
John K Pate <j.k.pate@sms.ed.ac.uk>
2010-11-05 19:00:04 +0000

    headway on HMM

--- testHMM.scala
+++ testHMM.scala
@@ -1,13 +1,16 @@
 package ProsodicParsing.examples;
 
+import collection.immutable.HashSet
 import ProsodicParsing.Types.HMM
 
 
 object testHMM {
   def main( args:Array[String] ) {
-    val t = new HMM( 5 )
+    val t = new HMM( 3 , HashSet("whoa","wait","what","?") )
 
-    println( t.TransitionMatrix(0)(1) )
+    println( t.TransitionMatrix("Q1")("Q2") )
+    t.buildHMM( Array("whoa","wait","what","?")  )
+    t.randomize(1)
     t.buildHMM( Array("whoa","wait","what","?") )
   }
 }

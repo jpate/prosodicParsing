d092cff688e95d49bb512bcc1950d4fa603d0ae7
John K Pate <j.k.pate@sms.ed.ac.uk>
2011-02-15 15:59:06 +0000

    manage runners by git

---
+++ src/main/scala/runners/CoupledChunker.scala
@@ -0,0 +1,146 @@
+package ProsodicParsing.runners
+import ProsodicParsing.HMMs.PlainHMM
+import ProsodicParsing.HMMs.CoupledHMM
+import ProsodicParsing.HMMs.HMMMaster
+import ProsodicParsing.HMMs.HMMActor
+import ProsodicParsing.HMMs.EvaluatingMaster
+import ProsodicParsing.types._
+import akka.actor.Actor.actorOf
+import akka.actor.ActorRef
+
+
+object CoupledChunker {
+  def main( args:Array[String]) {
+    import scala.math.log
+
+    val dataPath = args(0)
+    val testDataPath = args(1)
+    val convergenceTolerance = args(2).toDouble
+    val numProsodicStates = args(3).toInt
+    val numHMMs = args(4).toInt
+    val randSeed = if(args.length > 5 ) args(5).toInt else 15
+
+    //val obieCoding = Array( "O", "B", "I", "E" )
+    val obieCoding = Array( "B", "E", "I", "O" )
+
+    val hiddenStates =
+      obieCoding.flatMap{ obieCode =>
+        (0 to (numProsodicStates-1)).map{ y =>
+          HiddenStatePair( obieCode, "P_"+y )
+        }
+      }.toSet
+
+
+    val chunkingStates = obieCoding.map{ HiddenState( _ ) }.toSet
+
+    val transitionsToZero = hiddenStates.flatMap{ fromTransition =>
+      fromTransition match {
+        case HiddenStatePair( "O", _ ) =>
+          List(
+            Tuple2( fromTransition, HiddenState( "I" ) ),
+            Tuple2( fromTransition, HiddenState( "E" ) )
+          )
+        case HiddenStatePair( "B", _ ) =>
+          List(
+            Tuple2( fromTransition, HiddenState( "O" ) ),
+            Tuple2( fromTransition, HiddenState( "B" ) )
+          )
+        case HiddenStatePair( "I", _ ) =>
+          List(
+            Tuple2( fromTransition, HiddenState( "O" ) ),
+            Tuple2( fromTransition, HiddenState( "B" ) )
+          )
+        case HiddenStatePair( "E", _ ) =>
+          List(
+            Tuple2( fromTransition, HiddenState( "I" ) ),
+            Tuple2( fromTransition, HiddenState( "E" ) )
+          )
+      }
+    }.toSet
+
+    val initialStatesToZero = hiddenStates.filter{ hiddenState =>
+      hiddenState match {
+        case HiddenStatePair( "I", _ ) => true
+        case HiddenStatePair( "E", _ ) => true
+        case _ => false
+      }
+    }
+
+
+    val chunkingTransitions =
+      new ConditionalLogProbabilityDistribution( hiddenStates, chunkingStates )
+
+    chunkingTransitions.randomize( randSeed, 10 )
+
+    chunkingTransitions.zeroAll( transitionsToZero )
+
+    chunkingTransitions.normalize
+
+
+    val corpus = io.Source.fromFile( dataPath ).getLines().toList.map{ rawString =>
+      val tokenized = rawString.split(" ").toList
+        tokenized.tail.map{ w =>
+          val Array( word, prosody ) = w.split( "#" )
+          ObservedStatePair( word, prosody )
+        }
+    }.filter{ s => s.size > 2 }//&& s.size < 20 }
+
+    println( corpus.size + " training sentences" )
+
+    val testCorpus = io.Source.fromFile( testDataPath ).getLines().toList.map{ rawString =>
+      val tokenized = rawString.split(" ").toList
+      ViterbiString(
+        tokenized.head,
+        tokenized.tail.map{ w =>
+          val Array( word, prosody ) = w.split( "#" )
+          ObservedStatePair( word, prosody )
+        }
+      )
+    }.filter{ s => s.size > 2 }//&& s.size < 25 }
+
+    println( testCorpus.size + " dev sentences" )
+
+    val observationTypes = Set( corpus.flatten).flatten.toSet
+
+    //println( hiddenStates.size + " hidden states: " + hiddenStates.mkString("",", ",".") )
+
+    // println( observationTypes.size + " observed types:\n" +
+    // observationTypes.mkString("\t","\n\t","\n" ) )
+
+    val manager = actorOf(
+        new CoupledHMM(hiddenStates,observationTypes,"Master") with HMMMaster[HiddenStatePair,ObservedStatePair]
+        with EvaluatingMaster[HiddenStatePair,ObservedStatePair] {
+        val trainingData = corpus
+
+        println( "creating " + numHMMs + " HMMs")
+        var hmms = (0 to (numHMMs-1)).map{ n =>
+          actorOf( new CoupledHMM( hiddenStates, observationTypes.toSet, n.toString )
+            with HMMActor[HiddenStatePair,ObservedStatePair]).start
+        }.toList
+        println( "Made " + hmms.size + " HMMs")
+
+        val viterbiHMM = actorOf( new CoupledHMM( hiddenStates, observationTypes.toSet, "viterbi" ) with
+        HMMActor[HiddenStatePair,ObservedStatePair] )
+
+        randomize( randSeed, 10 )
+        transitionMatrixA.zeroAll( transitionsToZero )
+        initialStateProbabilities.zeroAll( initialStatesToZero )
+
+        val frequency = 4
+        val testSet = testCorpus
+
+        def converged( iterations:Int, deltaLogProb:Double ) =
+          iterations > 100 || ( math.abs( deltaLogProb ) < 0.01 && iterations > 15 )
+      }
+    )
+
+
+    manager.start
+    manager ! Initialize
+    println( "Starting HMM: ")
+    manager.start()
+  }
+}
+
+
+
